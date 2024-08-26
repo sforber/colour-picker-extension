047c8f374ea64f130d3c199c6903207287d51cc4
Scott Forber <scottforber@gmail.com>
2024-08-26 21:05:43 +0100

    Add unlimited colour history and copy functionality

--- main.js
+++ main.js
@@ -2,36 +2,24 @@ const eyeDropper = new EyeDropper();
 const pickerButton = document.getElementById("openPicker");
 const resultTable = document.getElementById("resultTable");
 const savedColours = [];
-const maxColours = 6;
 
 pickerButton.addEventListener('click', function () {
     eyeDropper.open()
         .then(res => {
             savedColours.unshift(res.sRGBHex);
 
-            if (savedColours.length > maxColours) { 
-                resultTable.deleteRow(maxColours - 1)
-                savedColours.length = maxColours;
-            };
-
             let colour = savedColours[0];
             var row = resultTable.insertRow(0);
             var cell = row.insertCell(0);
             var cell1 = row.insertCell(1);
             var cell2 = row.insertCell(2);
+
             cell.innerHTML = `<div style=\"height: 44px; width: 44px; background-color: ${colour}\"></div>`
             cell1.innerText = colour
-            cell2.innerHTML = "<button class=\"copyButton\" onclick=copyToClipboard()><i class=\"fa-regular fa-copy\" style=\"color: #2d2d2d;\"></i></button>"
-            console.log(colour)
-                
-           
-            console.log(savedColours)
+            cell2.innerHTML = `<button class=\"copyButton\"><svg width=\"100%\" height=\"100%\" viewBox=\"0 0 24 24\" fill=\"#f5f5f5\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"M12 11V17M12 17L9 14M12 17L15 14M16 4C16.93 4 17.395 4 17.7765 4.10222C18.8117 4.37962 19.6204 5.18827 19.8978 6.22354C20 6.60504 20 7.07003 20 8V17.2C20 18.8802 20 19.7202 19.673 20.362C19.3854 20.9265 18.9265 21.3854 18.362 21.673C17.7202 22 16.8802 22 15.2 22H8.8C7.11984 22 6.27976 22 5.63803 21.673C5.07354 21.3854 4.6146 20.9265 4.32698 20.362C4 19.7202 4 18.8802 4 17.2V8C4 7.07003 4 6.60504 4.10222 6.22354C4.37962 5.18827 5.18827 4.37962 6.22354 4.10222C6.60504 4 7.07003 4 8 4M9.6 6H14.4C14.9601 6 15.2401 6 15.454 5.89101C15.6422 5.79513 15.7951 5.64215 15.891 5.45399C16 5.24008 16 4.96005 16 4.4V3.6C16 3.03995 16 2.75992 15.891 2.54601C15.7951 2.35785 15.6422 2.20487 15.454 2.10899C15.2401 2 14.9601 2 14.4 2H9.6C9.03995 2 8.75992 2 8.54601 2.10899C8.35785 2.20487 8.20487 2.35785 8.10899 2.54601C8 2.75992 8 3.03995 8 3.6V4.4C8 4.96005 8 5.24008 8.10899 5.45399C8.20487 5.64215 8.35785 5.79513 8.54601 5.89101C8.75992 6 9.03995 6 9.6 6Z\" stroke=\"#000000\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\"/></svg></button>`
+            cell2.addEventListener('click', () => {navigator.clipboard.writeText(colour)})
         })
         .catch(err => {
             console.log("Cancelled");
         })
 })
-
-function copyToClipboard() {
-    navigator.clipboard.writeText(result.innerText.toUpperCase());
-}
