9fe319bbed904f10f51c4f2e956307221244078d
Scott Forber <scottforber@gmail.com>
2024-08-26 17:07:02 +0100

    Remove pasting functionality and add colour history.

--- main.js
+++ main.js
@@ -1,31 +1,37 @@
-const pasteArea = document.getElementById("pasteArea");
-const imgArea = document.getElementById("imgArea");
-const result = document.getElementById("result");
-
-pasteArea.addEventListener("paste", (event) => {
-    event.preventDefault();
-    let pastedItems = (event.clipboardData || window.clipboardData);
-    let pastedItem = pastedItems.items[0];
-    if (pastedItem.type.indexOf("image") !== -1) {
-        const blob = pastedItem.getAsFile();
-        const reader = new FileReader();
-        reader.addEventListener('load', function () {
-            imgArea.src = this.result
-        })
-        reader.readAsDataURL(blob)
-    }
-})
-
-const eyeDropper = new EyeDropper()
+const eyeDropper = new EyeDropper();
 const pickerButton = document.getElementById("openPicker");
+const resultTable = document.getElementById("resultTable");
+const savedColours = [];
+const maxColours = 6;
 
 pickerButton.addEventListener('click', function () {
     eyeDropper.open()
-    .then(res => {
-        result.innerText = res.sRGBHex
-        console.log(res.sRGBHex)
-    })
-    .catch(err => {
-        console.log("Cancelled")
-    })
+        .then(res => {
+            savedColours.unshift(res.sRGBHex);
+
+            if (savedColours.length > maxColours) { 
+                resultTable.deleteRow(maxColours - 1)
+                savedColours.length = maxColours;
+            };
+
+            let colour = savedColours[0];
+            var row = resultTable.insertRow(0);
+            var cell = row.insertCell(0);
+            var cell1 = row.insertCell(1);
+            var cell2 = row.insertCell(2);
+            cell.innerHTML = `<div style=\"height: 44px; width: 44px; background-color: ${colour}\"></div>`
+            cell1.innerText = colour
+            cell2.innerHTML = "<button class=\"copyButton\" onclick=copyToClipboard()><i class=\"fa-regular fa-copy\" style=\"color: #2d2d2d;\"></i></button>"
+            console.log(colour)
+                
+           
+            console.log(savedColours)
+        })
+        .catch(err => {
+            console.log("Cancelled");
+        })
 })
+
+function copyToClipboard() {
+    navigator.clipboard.writeText(result.innerText.toUpperCase());
+}
